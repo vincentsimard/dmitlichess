f1b565aaa9642360482e74696a1a35c01d8f5be5
Vincent Simard <vincent@vincentsimard.com>
2014-08-01 21:56:01 -0400

    Preloading sounds

--- app/scripts/dmitlichess.js
+++ app/scripts/dmitlichess.js
@@ -1,22 +1,77 @@
 'use strict';
 
+// @TODO: Queue sounds so Dmitry doesn't talk over himself
+//        see http://blogs.msdn.com/b/ie/archive/2011/05/13/unlocking-the-power-of-html5-lt-audio-gt.aspx
+
+// @TODO: Play random bits of Komarov at times ("yes", "unbelievable", etc.)
+
+// @TODO: Change the default yeoman logo to Komarov
+
+// @TODO: Preload sounds
+
+
+
 var squares = document.querySelectorAll('#lichess .lichess_board .lcs');
 var moveEmitter = new MoveEmitter(squares);
 
 var sounds = {};
 
-var initDmitry = function() {
-  var makeAudio = function(file, volume) {
-    console.log(chrome.extension.getURL('ogg/' + file));
+var makeAudio = function(file, volume) {
+  console.log(chrome.extension.getURL('ogg/' + file));
 
-    var audio = new Audio(chrome.extension.getURL('ogg/' + file));
-    audio.volume = volume;
+  var audio = new Audio(chrome.extension.getURL('ogg/' + file));
+  audio.volume = volume;
 
-    return audio;
-  };
+  return audio;
+};
 
-  sounds.e4 = makeAudio('c5.ogg', 1);
-  sounds.d4 = makeAudio('Nf3.ogg', 1);
+// @TODO: Is there a way to read file list in ./ogg instead of looping?
+// @TODO: Refactor this. No point in looping a brazilian times
+// @TODO: Handle error when trying to load a file that does not exist
+var preloadSounds = function() {
+  var pieces = ['', 'N', 'B', 'R', 'Q', 'K'];
+  var files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  var ranks = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
+  var i, j, k, notation, captureNotation;
+
+  // Moves
+  for (i=0; i<pieces.length; i++) {
+    for (j=0; j<files.length; j++) {
+      for (k=0; k<ranks.length; k++) {
+        notation = pieces[i] + files[j] + ranks[k];
+        sounds[notation] = makeAudio(notation + '.ogg', 1);
+      }
+    }
+  }
+
+  // Captures (pieces)
+  for (i=1; i<pieces.length; i++) {
+    for (j=0; j<files.length; j++) {
+      for (k=0; k<ranks.length; k++) {
+        notation = pieces[i] + 'x' + files[j] + ranks[k];
+        sounds[notation] = makeAudio(notation + '.ogg', 1);
+      }
+    }
+  }
+
+  // Captures (pawns)
+  for (i=0; i<files.length; i++) {
+    for (j=0; j<files.length; j++) {
+      for (k=0; k<ranks.length; k++) {
+        if (Math.abs(i-j) === 1) {
+          notation = files[i] + pieces[0] + 'x' + files[j] + ranks[k];
+          sounds[notation] = makeAudio(notation + '.ogg', 1);
+        }
+      }
+    }
+  }
+
+  sounds[notation] = makeAudio('0-0.ogg', 1);
+  sounds[notation] = makeAudio('0-0-0.ogg', 1);
+  
+  console.log(Object.keys(sounds));
+  console.log(Object.keys(sounds).length);
 };
 
 // @TODO: Doesn't work
@@ -29,22 +84,21 @@ var disableDefaultSounds = function() {
 };
 
 var unleashDmitry = function() {
-  $('#lichess').on('move', function(event, notation) {
-    console.log('move', notation);
+  $('#lichess').on('move capture', function(event, notation) {
+    // if (!sounds[notation]) {
+    //   sounds[notation] = makeAudio(notation + '.ogg', 1);
+    // }
 
-    sounds.e4.play();
-  });
-
-  $('#lichess').on('capture', function(event, notation) {
-    console.log('capture', notation);
-
-    sounds.d4.play();
+    if (sounds[notation]) {
+      sounds[notation].play();
+    }
   });
 };
 
 var init = function() {
-  initDmitry();
+  preloadSounds();
   disableDefaultSounds();
+
   unleashDmitry();
 };
 
