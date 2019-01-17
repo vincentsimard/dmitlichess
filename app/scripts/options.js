b4708fc62b6b53120ee7dcb7d8fb247439a47bab
Vincent Simard <vincent@vincentsimard.com>
2019-01-17 02:15:40 -0500

    Use es6 class for popup and options

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,8 +1,8 @@
-(function(browser, Utils) {
-  'use strict';
+'use strict';
 
-  const OptionsCtrl = {
-    elements: {
+class OptionsCtrl {
+  constructor() {
+    this.elements = {
       enabled: document.getElementById('enabled'),
       volume: document.getElementById('volume'),
       commentators: document.querySelectorAll('input[name="commentator"]'),
@@ -12,72 +12,72 @@
       status: document.getElementById('status'),
       saveButton: document.getElementById('save'),
       defaultButton: document.getElementById('default')
-    },
+    };
+  }
 
-    save: function(showStatus = true) {
-      const doSaved = ()=> {
-        Utils.sendSaveMessage();
+  save(showStatus = true) {
+    const doSaved = () => {
+      Utils.sendSaveMessage();
 
-        if (showStatus) {
-          // Update status to let user know options were saved.
-          const status = this.elements.status;
+      if (showStatus) {
+        // Update status to let user know options were saved.
+        const status = this.elements.status;
 
-          status.textContent = 'Options saved. Please refresh your lichess.org page';
-          status.classList.remove('faded');
+        status.textContent = 'Options saved. Please refresh your lichess.org page';
+        status.classList.remove('faded');
 
-          setTimeout(()=> status.classList.add('faded'), 5000);
-        }
-      };
+        setTimeout(()=> status.classList.add('faded'), 5000);
+      }
+    };
 
-      UserPrefs.saveOptions({
-        commentator:  document.querySelector('input[name="commentator"]:checked').value,
-        enabled:      this.elements.enabled.checked,
-        volume:       this.elements.volume.value,
-        miscInterval: this.elements.miscInterval.value,
-        fillInterval: this.elements.fillInterval.value,
-        longTimeout:  this.elements.longTimeout.value
-      }).then(doSaved);
-    },
+    UserPrefs.saveOptions({
+      commentator:  document.querySelector('input[name="commentator"]:checked').value,
+      enabled:      this.elements.enabled.checked,
+      volume:       this.elements.volume.value,
+      miscInterval: this.elements.miscInterval.value,
+      fillInterval: this.elements.fillInterval.value,
+      longTimeout:  this.elements.longTimeout.value
+    }).then(doSaved);
+  }
 
-    reset: function() {
-      document.getElementById('commentator_' + UserPrefs.defaults.commentator).checked = true;
-      this.elements.enabled.checked = UserPrefs.defaults.enabled;
-      this.elements.miscInterval.value = UserPrefs.defaults.miscInterval;
-      this.elements.fillInterval.value = UserPrefs.defaults.fillInterval;
-      this.elements.longTimeout.value = UserPrefs.defaults.longTimeout;
+  reset() {
+    document.getElementById('commentator_' + UserPrefs.defaults.commentator).checked = true;
+    this.elements.enabled.checked = UserPrefs.defaults.enabled;
+    this.elements.miscInterval.value = UserPrefs.defaults.miscInterval;
+    this.elements.fillInterval.value = UserPrefs.defaults.fillInterval;
+    this.elements.longTimeout.value = UserPrefs.defaults.longTimeout;
 
-      this.save();
-    },
+    this.save();
+  }
 
-    // Restores select box and checkbox state using the preferences storage.
-    restore: function() {
-      // Default values
-      UserPrefs.getOptions().then((items)=> {
-        document.getElementById('commentator_' + items.commentator).checked = true;
-        this.elements.enabled.checked = items.enabled;
-        this.elements.volume.value = items.volume;
-        this.elements.miscInterval.value = items.miscInterval;
-        this.elements.fillInterval.value = items.fillInterval;
-        this.elements.longTimeout.value = items.longTimeout;
-      });
-    },
+  // Restores select box and checkbox state using the preferences storage.
+  restore() {
+    // Default values
+    UserPrefs.getOptions().then((items)=> {
+      document.getElementById('commentator_' + items.commentator).checked = true;
+      this.elements.enabled.checked = items.enabled;
+      this.elements.volume.value = items.volume;
+      this.elements.miscInterval.value = items.miscInterval;
+      this.elements.fillInterval.value = items.fillInterval;
+      this.elements.longTimeout.value = items.longTimeout;
+    });
+  }
 
-    init: function() {
-      document.addEventListener('DOMContentLoaded', ()=> this.restore());
+  init() {
+    document.addEventListener('DOMContentLoaded', () => this.restore());
 
-      this.elements.enabled.addEventListener('change', ()=> this.save(false));
+    this.elements.enabled.addEventListener('change', () => this.save(false));
 
-      this.elements.saveButton.addEventListener('click', ()=> this.save());
-      this.elements.defaultButton.addEventListener('click', ()=> this.reset());
+    this.elements.saveButton.addEventListener('click', () => this.save());
+    this.elements.defaultButton.addEventListener('click', () => this.reset());
 
-      // Play a random commentary when a commentator is selected
-      Array.prototype.forEach.call(this.elements.commentators, (item)=> {
-        const listener = ()=> Utils.audio.play('misc', item.value, this.elements.volume.value);
-        item.addEventListener('click', listener);
-      });
-    }
-  };
+    // Play a random commentary when a commentator is selected
+    Array.prototype.forEach.call(this.elements.commentators, item => {
+      const listener = () => Utils.audio.play('misc', item.value, this.elements.volume.value);
+      item.addEventListener('click', listener);
+    });
+  }
+}
 
-  const ctrl = Object.create(OptionsCtrl);
-  ctrl.init();
-})(browser, Utils);
+window.optionsCtrl = new OptionsCtrl();
+window.optionsCtrl.init();
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,98 +1,98 @@
-(function(browser, sounds, Utils) {
-  'use strict';
+'use strict';
 
-  const PopupCtrl = {
-    options: UserPrefs.defaults,
+class PopupCtrl {
+  constructor(options) {
+    this.options = UserPrefs.defaults;
+  }
 
-    generateMiscList: function() {
-      const s = sounds[this.options.commentator];
-      const soundFiles = []
-        .concat(s.misc)
-        .concat(s.check)
-        .concat(s.checkmate)
-        .concat(s.stalemate)
-        .concat(s.draw)
-        .concat(s.resign)
-        .concat(s.start)
-        .concat(s.name)
-        .concat(s.check)
-        .concat(s.fill)
-        .concat(s.long)
-        .concat(s.signoff)
-        .filter((n)=> !!n); // Remove undefined entries (if sounds don't exist for one category)
+  generateMiscList() {
+    const s = sounds[this.options.commentator];
+    const soundFiles = []
+      .concat(s.misc)
+      .concat(s.check)
+      .concat(s.checkmate)
+      .concat(s.stalemate)
+      .concat(s.draw)
+      .concat(s.resign)
+      .concat(s.start)
+      .concat(s.name)
+      .concat(s.check)
+      .concat(s.fill)
+      .concat(s.long)
+      .concat(s.signoff)
+      .filter((n)=> !!n); // Remove undefined entries (if sounds don't exist for one category)
 
-      const soundboard = document.querySelector('#soundboard');
-      const selectList = document.createElement('select');
-      selectList.id = 'miscList';
+    const soundboard = document.querySelector('#soundboard');
+    const selectList = document.createElement('select');
+    selectList.id = 'miscList';
 
-      const toDisplayName = filename => filename
-        .replace(/misc_|long_|\.ogg/g, '')
-        .replace(/_/g, ' ');
+    const toDisplayName = filename => filename
+      .replace(/misc_|long_|\.ogg/g, '')
+      .replace(/_/g, ' ');
 
-      const createOption = filename => new Option(toDisplayName(filename), filename);
+    const createOption = filename => new Option(toDisplayName(filename), filename);
 
-      selectList.add(new Option('', ''));
-      soundFiles.forEach(sound => selectList.add(createOption(sound)));
+    selectList.add(new Option('', ''));
+    soundFiles.forEach(sound => selectList.add(createOption(sound)));
 
-      soundboard.appendChild(selectList);
-    },
+    soundboard.appendChild(selectList);
+  }
 
-    addListeners: function() {
-      let keyModifier = '';
-      const {commentator, volume} = this.options;
-      const squares = Array.from(document.querySelectorAll('#board .square'));
+  addListeners() {
+    let keyModifier = '';
+    const {commentator, volume} = this.options;
+    const squares = Array.from(document.querySelectorAll('#board .square'));
 
-      const createSquareEventListener = event => {
-        // @TODO: Can't listen to O-O, O-O-O sounds
-        const keys = ['N', 'B', 'R', 'Q', 'K'];
-        let notation = event.target.id;
+    const createSquareEventListener = event => {
+      // @TODO: Can't listen to O-O, O-O-O sounds
+      const keys = ['N', 'B', 'R', 'Q', 'K'];
+      let notation = event.target.id;
 
-        if (event.shiftKey) { notation = 'x' + notation; }
-        if (keys.includes(keyModifier.toUpperCase())) { notation = keyModifier + notation; }
+      if (event.shiftKey) { notation = 'x' + notation; }
+      if (keys.includes(keyModifier.toUpperCase())) { notation = keyModifier + notation; }
 
-        Utils.audio.play(notation, commentator, volume);
-      };
+      Utils.audio.play(notation, commentator, volume);
+    };
 
-      squares.forEach(square => square.addEventListener('click', createSquareEventListener));
+    squares.forEach(square => square.addEventListener('click', createSquareEventListener));
 
-      document.addEventListener('keydown', event => {
-        keyModifier = String.fromCharCode(event.keyCode);
-      });
+    document.addEventListener('keydown', event => {
+      keyModifier = String.fromCharCode(event.keyCode);
+    });
 
-      document.addEventListener('keyup', () => {
-        keyModifier = '';
-      });
+    document.addEventListener('keyup', () => {
+      keyModifier = '';
+    });
 
-      // "Play a random commentary" link
-      document.getElementById('randomMisc').addEventListener('click', () => {
-        Utils.audio.play('misc', this.options.commentator, this.options.volume);
-      });
+    // "Play a random commentary" link
+    document.getElementById('randomMisc').addEventListener('click', () => {
+      Utils.audio.play('misc', this.options.commentator, this.options.volume);
+    });
 
-      // Full sound list dropdown
-      document.getElementById('miscList').addEventListener('change', event => {
-        Utils.audio.play(event.target.value, this.options.commentator, this.options.volume, false);
-      });
+    // Full sound list dropdown
+    document.getElementById('miscList').addEventListener('change', event => {
+      Utils.audio.play(event.target.value, this.options.commentator, this.options.volume, false);
+    });
 
-      document.getElementById('enabled').addEventListener('change', event => {
-        UserPrefs.saveOptions({ enabled: event.target.checked }).then(Utils.sendSaveMessage);
-      });
-    },
+    document.getElementById('enabled').addEventListener('change', event => {
+      UserPrefs.saveOptions({ enabled: event.target.checked }).then(Utils.sendSaveMessage);
+    });
+  }
 
-    init: function() {
-      if (!sounds) { return; }
-      if (!browser.storage) { return; }
+  init() {
+    if (!sounds) { return; }
+    if (!browser.storage) { return; }
 
-      UserPrefs.getOptions().then(items => {
-        this.options = items;
+    UserPrefs.getOptions().then(items => {
+      this.options = items;
 
-        document.getElementById('enabled').checked = this.options.enabled;
+      document.getElementById('enabled').checked = this.options.enabled;
 
-        this.generateMiscList();
-        this.addListeners();
-      });
-    }
-  };
+      this.generateMiscList();
+      this.addListeners();
+    });
+  }
+}
 
-  const ctrl = Object.create(PopupCtrl);
-  ctrl.init();
-})(browser, sounds, Utils);
+window.popupCtrl = new PopupCtrl();
+window.popupCtrl.init();
